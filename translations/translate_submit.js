ec3ac2539281151389b610cdfbe6907cba5ccc1d
Douile <tom.k.hipwell@gmail.com>
2018-07-11 21:00:26 +0100

    add save function

--- translations/translate_submit.js
+++ translations/translate_submit.js
@@ -46,6 +46,20 @@ function parseData(data) {
     code: code
   };
 }
+var saveAs = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, fileName) {
+        var json = JSON.stringify(data),
+            blob = new Blob([json], {type: "octet/stream"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
 function save() {
   var data = parseSearch(location.search);
   var parsed = parseData(data);
