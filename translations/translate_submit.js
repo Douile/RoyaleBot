7a392772131bec6afb785d2affdc3e44c59fa759
Douile <tom.k.hipwell@gmail.com>
2018-07-11 21:03:24 +0100

    Fix bug

--- translations/translate_submit.js
+++ translations/translate_submit.js
@@ -50,10 +50,8 @@ var saveAs = (function () {
     var a = document.createElement("a");
     document.body.appendChild(a);
     a.style = "display: none";
-    return function (data, fileName) {
-        var json = JSON.stringify(data),
-            blob = new Blob([json], {type: "octet/stream"}),
-            url = window.URL.createObjectURL(blob);
+    return function (blob, fileName) {
+        var url = window.URL.createObjectURL(blob);
         a.href = url;
         a.download = fileName;
         a.click();
