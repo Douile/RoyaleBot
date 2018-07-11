9b5e1a7f90d2853d9d4b43a2176bd0a96260be49
Douile <tom.k.hipwell@gmail.com>
2018-07-11 20:55:37 +0100

    Add submit

---
+++ translations/translate_submit.js
@@ -0,0 +1,59 @@
+const MSG_PREFIX = 'message_';
+
+function parseSearch(string) {
+  if (string.startsWith('?')) {
+    string.substr(1);
+  }
+  var values = string.split('&');
+  var data = {};
+  for (var i=0;i<values.length;i++) {
+    var args = values[i].split('=');
+    var key = args[0];
+    var value = args[1];
+    data[key] = value;
+  }
+  return data;
+}
+function parseData(data) {
+  var manifest = {
+    name: '',
+    name_en: '',
+    author: '',
+    author_name: ''
+  };
+  var messages = {};
+  var code;
+  for (var key in data) {
+    var value = data[key];
+    if (key == 'author_name') {
+      manifest['author_name'] = value;
+    } else if (key == 'author') {
+      manifest['author'] = value;
+    } else if (key == 'lang_name') {
+      manifest['name'] = value;
+    } else if (key == 'lang_name_en') {
+      manifest['name_en'] = value;
+    } else if (key == 'lang_code') {
+      code = value;
+    } else if (key.startsWith(MSG_PREFIX)) {
+      var msgKey = key.substr(MSG_PREFIX.length);
+      messages[msgKey] = value;
+    }
+  }
+  return {
+    manifest: manifest,
+    messages: messages,
+    code: code
+  };
+}
+function save() {
+  var data = parseSearch(location.search);
+  var parsed = parseData(data);
+  var zip = new JSZip();
+  var folder = zip.folder(parsed.code);
+  folder.file('manifest.json',JSON.parse(parsed.manifest));
+  folder.file('messages.json',JSON.parse(parsed.messages));
+  zip.generateAsync({type:'blob'}).then(function(content) {
+    saveAs(content,`${parsed.code}.zip`);
+  })
+}
