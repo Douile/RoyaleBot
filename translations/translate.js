1e8dc4055871feb27f9fec480a0af67ee020051a
Douile <tom.k.hipwell@gmail.com>
2018-09-14 18:18:12 +0100

    No need for translate_submit

--- translations/translate.js
+++ translations/translate.js
@@ -35,6 +35,20 @@ function onLoad() {
     select.appendChild(option);
   }
   select.addEventListener('change',selectNew);
+  var form = document.getElementById('translate_form');
+  form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    var data = {},
+    doms = document.getElementsByTagName('input'),
+    i = 0;
+    doms.forEach((dom) => {
+      i += 1;
+      data[dom.getAttribute('name')] = dom.value;
+      if (i >= doms.length) {
+        save(data);
+      }
+    })
+  })
   loadLanguage('en',root);
 }
 
@@ -61,4 +75,61 @@ function loadLanguage(code,root) {
     }
   })
 }
+
+function parseData(data) {
+  var manifest = {
+    name: '',
+    name_en: '',
+    author: '',
+    author_name: ''
+  };
+  var messages = {};
+  var code;
+  for (var key in data) {
+    var value = data[key];
+    if (key == 'author_name') {
+      manifest['author_name'] = value;
+    } else if (key == 'author') {
+      manifest['author'] = value;
+    } else if (key == 'lang_name') {
+      manifest['name'] = value;
+    } else if (key == 'lang_name_en') {
+      manifest['name_en'] = value;
+    } else if (key == 'lang_code') {
+      code = value;
+    } else if (key.startsWith(MSG_PREFIX)) {
+      var msgKey = key.substr(MSG_PREFIX.length);
+      messages[msgKey] = {'message':value};
+    }
+  }
+  return {
+    manifest: manifest,
+    messages: messages,
+    code: code
+  };
+}
+var saveAs = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (blob, fileName) {
+        var url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
+function save(data) {
+  var parsed = parseData(data);
+  var zip = new JSZip();
+  var folder = zip.folder(parsed.code);
+  folder.file('manifest.json',JSON.stringify(parsed.manifest));
+  folder.file('messages.json',JSON.stringify(parsed.messages));
+  zip.generateAsync({type:'blob'}).then(function(content) {
+    saveAs(content,`${parsed.code}.zip`);
+  })
+}
+save();
+
 window.addEventListener('load',onLoad);
